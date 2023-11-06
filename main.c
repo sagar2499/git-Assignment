1536c05958b837c0e8617a49fe10c092868c96b1
sagar2499 <122062481+sagar2499@users.noreply.github.com>
2023-11-07 05:26:55 +0530

    main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+pseudo code
+{
+Initial Function()
+{
+  //Sample Code
+}
+
+Initial Security()
+{
+ //Sample Code
+}
+
+
+
+}
+
+#security patch
+{
+        security patch for feature 1 branch
+
+#Security-Patch
+
+{
+        Security patch for feature 2 branch
+
+}
